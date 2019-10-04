b48929ef940169c0f06d009ac5694b9655157287
Daniel Murillo <daniel.murillo@sophossolutions.com>
2019-10-04 17:26:32 -0500

    Se expone servicio cliente

--- src/main/java/com/daniel/springboot/backend/apirest/models/services/ClienteServiceImpl.java
+++ src/main/java/com/daniel/springboot/backend/apirest/models/services/ClienteServiceImpl.java
@@ -3,11 +3,13 @@ package com.daniel.springboot.backend.apirest.models.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.daniel.springboot.backend.apirest.models.dao.IClienteDao;
 import com.daniel.springboot.backend.apirest.models.entity.Cliente;
 
+@Service
 public class ClienteServiceImpl implements IClienteService{
 	
 	@Autowired
@@ -17,6 +19,21 @@ public class ClienteServiceImpl implements IClienteService{
 	@Transactional(readOnly = true)
 	public List<Cliente> findAll() {
 		return (List<Cliente>) clienteDao.findAll();
+	}
+
+	@Override
+	public Cliente findById(Long id) {
+		return clienteDao.findById(id).orElse(null);
+	}
+
+	@Override
+	public Cliente save(Cliente cliente) {
+		return clienteDao.save(cliente);
+	}
+
+	@Override
+	public void delete(Long id) {
+		clienteDao.deleteById(id);
 	}
 
 }
